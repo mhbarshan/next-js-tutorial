87c5539a5b2e74248c494e0d7d5ce125e7cdd82a
mahamudul-hassan-bs <mahamudul.hassan@brainstation-23.com>
2025-11-30 01:30:48 +0600

    refactor: Remove unused dynamic rendering and TypeScript build error ignoring

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
   cacheComponents: true,
   images: {
     remotePatterns: [
